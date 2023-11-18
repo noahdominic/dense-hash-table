d17447c69610b2b2f5521b5b805bce3a93a682c1
noahdominic <noahdmnc@gmail.com>
2023-11-18 09:25:18 +0800

    write: Update `main.c` to generate random keys and values for testing

--- main.c
+++ main.c
@@ -1,131 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #include "densehashtable.h"
 #include "rustyc.h"
 
-#define NUM_ITEMS 2
-
+#define NUM_ITEMS 12
 unsigned long long collision_count;
 
-int main()
+// Function to generate a random alphanumeric string of length between minLen
+// and maxLen
+void
+generateRandomName(char* name, int minLen, int maxLen);
+
+
+int
+main(int argc, char *argv[])
 {
-    const char *names[] = {
-            "Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Henry",
-            "Ivy", "Jack", "Kate", "Liam", "Mia", "Noah", "Olivia", "Peter",
-            "Quinn", "Riley", "Sam", "Tyler", "Uma", "Violet", "Will", "Xander",
-            "Yara", "Zane", "Ava", "Ben", "Chloe", "Daniel", "Emily", "Felicia"};
+  printf("%i\n", argc);
+  if (argc < 2) {
+    printf("Usage: %s <number>\n", argv[0]);
+    exit(1);
+  }
 
-    int values[] = {
-            42, 57, 33, 78, 23, 56, 89, 64,
-            38, 51, 77, 45, 60, 30, 94, 68,
-            72, 55, 41, 79, 62, 87, 50, 63,
-            96, 75, 80, 53, 36, 67, 98, 69};
+  char *endptr;
+  long num_items = strtol(argv[1], &endptr, 10);
 
+  // Check for conversion errors
+  if (*endptr != '\0') {
+    printf("Invalid input: %s\n", argv[1]);
+    return 1;
+  }
 
-    Result res;
-    ResultOption resopt;
+  Result res;
+  ResultOption resopt;
 
-    for (int i = 0; i < 32; i++) {
-        struct DenseHashTable *dht = NULL;
-        if ((dht = dense_hash_table_init()) == NULL) {
-            continue;
-        }
+  srand(time(NULL));
 
-        for (int j = 0; j < i; j++) {
-            if (!(res = dense_hash_table_insert(dht, names[j], values[j])).is_ok) {
-                printf("Error %i: %s", res.error_code, res.error_message);
-            }
-        }
-
-        for (int j = 0; j < i; j++) {
-            if (!(resopt = dense_hash_table_delete(dht, names[j])).is_ok) {
-                printf("Error %i: %s", resopt.error_code, resopt.error_message);
-            }
-        }
-
-        printf("%u, %lld\n", i, collision_count);
-        dense_hash_table_destroy(dht);
+  struct DenseHashTable* dht = NULL;
+  char name[13];
+  char names[num_items][13];
+  int value = 0;
+  for (int i = 0; i < num_items; i++) {
+    if ((dht = dense_hash_table_init()) == NULL) {
+      continue;
     }
 
-    //    for (int i = 0; i < NUM_ITEMS; i++) {
-    //        Result res;
-    //
-    //        res = dense_hash_table_insert(dht, names[i], values[i]);
-    //
-    //        if (!res.is_ok) {
-    //            printf("Error %i: %s", res.error_code, res.error_message);
-    //            goto erase_all;
-    //        }
-    //
-    ////        res = dense_hash_table_print(dht);
-    ////        if (!res.is_ok) printf("Error %i: %s", res.error_code, res.error_message);
-    //    }
-    //
-    //    {
-    //        Result res;
-    //
-    //        res = dense_hash_table_insert(dht, names[4], values[4]);
-    //
-    //        if (!res.is_ok) {
-    //            printf("Error %i: %s\n", res.error_code, res.error_message);
-    //            if (res.error_code != ENTRY_ALREADY_EXISTS) {
-    //                goto erase_all;
-    //            }
-    //        }
-    //
-    ////        res = dense_hash_table_print(dht);
-    ////
-    ////        if (!res.is_ok) printf("Error %i: %s\n", res.error_code, res.error_message);
-    //    }
-    //
-    //    for (int i = 0; i < NUM_ITEMS; i++) {
-    //        const char *name = names[i];
-    //        ResultOption res2 = dense_hash_table_lookup(dht, name);
-    //
-    //        if (!res2.is_ok) {
-    //            printf("Error %i: %s\n", res2.error_code, res2.error_message);
-    //        }
-    ////        else if (!res2.value.is_some) {
-    ////            printf("\"%s\" is not in the hash table.\n", name);
-    ////        } else {
-    ////            printf("\"%s\" is: %i\n", name, res2.value.value);
-    ////        }
-    //    }
-    //
-    //    {
-    //        const char *name = NULL;
-    //        ResultOption res2 = dense_hash_table_lookup(dht, name);
-    //
-    //        if (!res2.is_ok) {
-    //            printf("Error %i: %s\n", res2.error_code, res2.error_message);
-    //        }
-    ////        else if (!res2.value.is_some) {
-    ////            printf("\"%s\" is not in the hash table.\n", name);
-    ////        } else {
-    ////            printf("\"%s\" is: %i\n", name, res2.value.value);
-    ////        }
-    //    }
-    //
-    //    for (int i = 0; i < NUM_ITEMS; i++) {
-    //        const char *name = names[i];
-    //        ResultOption res = dense_hash_table_delete(dht, names[i]);
-    //
-    //        if (!res.is_ok) {
-    //            printf("Error %i: %s", res.error_code, res.error_message);
-    //            goto erase_all;
-    //        }
-    //
-    //        if (!res.value.is_some) {
-    //            printf("\"%s\" is not in the hashtable\n", names[i]);
-    //        }
+    for (int j = 0; j < i; j++) {
+      generateRandomName(name, 5, 12);
+      strcpy(names[j], name);
+      value = rand();
 
-    //        Result res2 = dense_hash_table_print(dht);
-    //
-    //        if (!res2.is_ok) printf("Error %i: %s", res2.error_code, res2.error_message);
-    //    }
+      res = dense_hash_table_insert(dht, name, value);
+      if (!(res).is_ok) {
+        puts("Called from `main()`\n"
+             "Trying to: Call `dense_hash_table_insert()`\n");
+        printf("(j=%i): Error %i: %s\n", j, res.error_code, res.error_message);
+        goto erase_all;
+      }
+    }
+
+
+    for (int j = 0; j < i; j++) {
+      resopt = dense_hash_table_remove(dht, names[j]);
+      if (!(resopt).is_ok) {
+        puts("Called from `main()`\n"
+             "Trying to: Call `dense_hash_table_remove()`\n");
+        printf(
+          "(j=%i): Error %i: %s\n", j, resopt.error_code, resopt.error_message);
+        goto erase_all;
+      }
+    }
+
+
+    printf("%u, %lld\n", i, collision_count);
+
+    dense_hash_table_destroy(dht);
+  }
+
+  //    for (int i = 0; i < NUM_ITEMS; i++) {
+  //        Result res;
+  //
+  //        res = dense_hash_table_insert(dht, names[i], values[i]);
+  //
+  //        if (!res.is_ok) {
+  //            printf("Error %i: %s", res.error_code, res.error_message);
+  //            goto erase_all;
+  //        }
+  //
+  ////        res = dense_hash_table_print(dht);
+  ////        if (!res.is_ok) printf("Error %i: %s", res.error_code,
+  /// res.error_message);
+  //    }
+  //
+  //    {
+  //        Result res;
+  //
+  //        res = dense_hash_table_insert(dht, names[4], values[4]);
+  //
+  //        if (!res.is_ok) {
 
 erase_all:
 
-    return 0;
+  return 0;
+}
+
+
+void
+generateRandomName(char* name, int minLen, int maxLen)
+{
+  static const char charset[] =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  int length = rand() % (maxLen - minLen + 1) + minLen;
+
+  for (int i = 0; i < length; ++i) {
+    int index = rand() % (sizeof(charset) - 1);
+    name[i] = charset[index];
+  }
+
+  name[length] = '\0'; // Null-terminate the string
 }
