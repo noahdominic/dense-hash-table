623628c81414f7c6e662a59de6ae69e4b085e703
noahdominic <noahdmnc@gmail.com>
2023-11-18 10:17:09 +0800

    style: Update style for `densehashtable.h`

--- densehashtable/include/densehashtable.h
+++ densehashtable/include/densehashtable.h
@@ -1,6 +1,10 @@
 #pragma once
 
-#define FREE(ptr) do { free(ptr); ptr = 0; } while (0)
+#define FREE(ptr)                                                              \
+  do {                                                                         \
+    free(ptr);                                                                 \
+    ptr = 0;                                                                   \
+  } while (0)
 
 #define DHT_INIT_CAPACITY 8
 #define DHT_DEFAULT_GROWTH_RATE 2
@@ -9,7 +13,7 @@
 
 enum DhtErrorCode
 {
-  E_DHT_NULL,        // `dht` is NULL
+  E_DHT_NULL, // `dht` is NULL
   E_DHT_E_NULL,
   E_FUNC_PARAM_NULL, // A param called `key` is NULL
   E_ALLOC_FAIL,      // Result of `malloc`/`calloc`/`realloc` is NULL.
@@ -22,36 +26,38 @@ static const char* DhtErrorMessages[] = {
   "Result of `malloc`/`calloc`/`realloc` is NULL.\n"
 };
 
-
 struct DenseHashTableEntry
 {
-    char *key;
-    int value;
-    int hash;
+  char* key;
+  int value;
+  int hash;
 };
-
 
 struct DenseHashTable
 {
-    unsigned int size;
-    unsigned int capacity;
-    int *indices;
-    struct DenseHashTableEntry *entries;
+  unsigned int size;
+  unsigned int capacity;
+  int* indices;
+  struct DenseHashTableEntry* entries;
 };
-
 
 extern unsigned long long collision_count;
 
+Result calculate_hash(const char* key);
 
-Result calculate_hash(const char *key);
+void dense_hash_table_entry_cleanup(struct DenseHashTableEntry* entry);
+Result dense_hash_table_entry_set(struct DenseHashTableEntry* entry,
+                                  const char* key,
+                                  int value);
+Result dense_hash_table_entry_print(const struct DenseHashTableEntry* entry);
 
-void dense_hash_table_entry_destroy(struct DenseHashTableEntry *entry);
-Result dense_hash_table_entry_set(struct DenseHashTableEntry *entry, const char *key, int value);
-Result dense_hash_table_entry_print(const struct DenseHashTableEntry *entry);
-
-struct DenseHashTable *dense_hash_table_init();
-Result dense_hash_table_destroy(struct DenseHashTable *dht);
-Result dense_hash_table_print(const struct DenseHashTable *dht);
-Result dense_hash_table_insert(struct DenseHashTable *dht, const char *key, int value);
-ResultOption dense_hash_table_lookup(const struct DenseHashTable *dht, const char *key);
-ResultOption dense_hash_table_delete(struct DenseHashTable *dht, const char *key);
+struct DenseHashTable* dense_hash_table_init();
+Result dense_hash_table_destroy(struct DenseHashTable* dht);
+Result dense_hash_table_print(const struct DenseHashTable* dht);
+Result dense_hash_table_insert(struct DenseHashTable* dht,
+                               const char* key,
+                               int value);
+ResultOption dense_hash_table_lookup(const struct DenseHashTable* dht,
+                                     const char* key);
+ResultOption dense_hash_table_remove(struct DenseHashTable* dht,
+                                     const char* key);
