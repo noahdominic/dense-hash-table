e8efcad3d3312c0f49279b1d3843f8037b4e6193
noahdominic <noahdmnc@gmail.com>
2023-11-20 07:27:00 +0800

    fix: Add emergency exit for errors in main

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int
 main()
 {
+  int error_code = 0;
   const char* names[] = {
     "Awesome Etiquette",
     "Misquoting Jesus",
@@ -12,10 +13,15 @@ main()
   const int subscriber_counts[] = { 412098, 2345, 12908 };
 
   struct DenseHashTable* subscribers = dense_hash_table_init();
+  if (subscribers == NULL) {
+    error_code = -1;
+    goto emergency_exit;
+  }
 
   for (int i = 0; i < 3; i++) {
     if (!dense_hash_table_insert(subscribers, names[i], subscriber_counts[i])
            .is_ok) {
+      error_code = -1;
       goto emergency_exit;
     }
   }
@@ -24,5 +30,5 @@ main()
 
 emergency_exit:
   dense_hash_table_destroy(subscribers);
-  return 0;
+  return error_code;
 }
